66651cde0a38456fd56a7e1ea23f5228c0e5dfb6
kcherenkovv <kcherenkov9888@mail.ru>
2024-12-23 22:25:33 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="credit_default_analysis",
+    version="1.1b",
+    description="Credit default analysis package",
+    author="Max",
+    packages=find_packages(),
+    install_requires=[
+        # не забыть указать зависимости
+    ],
+    python_requires='>=3.9',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
