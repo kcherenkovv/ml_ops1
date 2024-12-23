19ce7f4493206706d104b7002c467f58f1dd1aa6
kcherenkovv <kcherenkov9888@mail.ru>
2024-12-23 22:33:06 +0300

    Change setup

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup
 
-with open("setup.py") as f:
-    setup(setup(f))
-
-class Setup(object):
-    def __init__(self, distribution, **kwargs):
-        self.distribution = distribution
-        self.kwargs = kwargs
-
-    def run(self):
-        self.distribution.run_pytest()
+def setup(distribution):
+    distribution.run_pytest()
